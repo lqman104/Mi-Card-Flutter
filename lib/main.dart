02fdec0fce9eababd44bd844155d5dfc40c56b4a
Luqman <lqman104@gmail.com>
2023-07-16 15:01:04 +0700

    add section contact

--- lib/main.dart
+++ lib/main.dart
@@ -14,6 +14,7 @@ class MyApp extends StatelessWidget {
         backgroundColor: Colors.teal,
         body: SafeArea(
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
             children: [
               CircleAvatar(
                 radius: 50,
@@ -22,8 +23,50 @@ class MyApp extends StatelessWidget {
               Text("Luqman Hakim",
                   style: TextStyle(
                       fontWeight: FontWeight.bold,
+                      fontFamily: "Pacifico",
                       color: Colors.white,
-                      fontSize: 40))
+                      fontSize: 40)),
+              Text("FLUTTER DEVELOPER",
+                  style: TextStyle(
+                      fontFamily: "SourceSan",
+                      color: Colors.teal.shade100,
+                      fontSize: 20,
+                      letterSpacing: 2.5,
+                      fontWeight: FontWeight.bold)),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.phone,
+                    color: Colors.teal,
+                    size: 30,
+                  ),
+                  title: Text(
+                    "098979879897",
+                    style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontSize: 20,
+                        fontFamily: "SourceSans"),
+                  ),
+                ),
+              ),
+              Card(
+                margin: EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+                child: ListTile(
+                  leading: Icon(
+                    Icons.email,
+                    color: Colors.teal,
+                    size: 30,
+                  ),
+                  title: Text(
+                    "lqman104@gmail.com",
+                    style: TextStyle(
+                        color: Colors.teal.shade900,
+                        fontSize: 20,
+                        fontFamily: "SourceSans"),
+                  ),
+                ),
+              )
             ],
           ),
         ),
