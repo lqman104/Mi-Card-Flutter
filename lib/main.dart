f0803f9a9e62c7a8c8f169e15308e1a21ce24a77
Luqman <lqman104@gmail.com>
2023-07-16 07:43:26 +0700

    try row and column

--- lib/main.dart
+++ lib/main.dart
@@ -13,13 +13,47 @@ class MyApp extends StatelessWidget {
       home: Scaffold(
         backgroundColor: Colors.red,
         body: SafeArea(
-          child: Container(
-            height: 100.0,
-            width: 100.0,
-            color: Colors.white,
-            padding: EdgeInsets.all(16),
-            margin: EdgeInsets.symmetric(vertical: 40.0, horizontal: 60.0),
-            child: Text("Luqman hakim"),
+          child: Column (
+            mainAxisSize: MainAxisSize.max,
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+              Container(
+                height: 100.0,
+                color: Colors.white,
+                padding: EdgeInsets.all(16),
+                margin: EdgeInsets.symmetric(horizontal: 20.0),
+                child: Text("Name: Luqman hakim"),
+              ),
+              SizedBox(height: 8),
+              Container(
+                height: 100.0,
+                color: Colors.white,
+                padding: EdgeInsets.all(16),
+                margin: EdgeInsets.symmetric(horizontal: 20.0),
+                child: Text("Type: Human"),
+              ),
+              SizedBox(height: 24),
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  Container(
+                    color: Colors.white,
+                    padding: EdgeInsets.all(16),
+                    child: Text("weight: \n85.0 Kg"),
+                  ),
+                  Container(
+                    color: Colors.white,
+                    padding: EdgeInsets.all(16),
+                    child: Text("height: \n180.0 Cm"),
+                  ),
+                  Container(
+                    color: Colors.white,
+                    padding: EdgeInsets.all(16),
+                    child: Text("ability: \nNgoding"),
+                  )
+                ],
+              )
+            ],
           ),
         ),
       ),
